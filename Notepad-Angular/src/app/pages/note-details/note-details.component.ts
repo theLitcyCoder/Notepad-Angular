3e8b38c23efd721deb47a1d3b53029471e3ceacf
Alicia Harewood <alicia.harewoodbcc@gmail.com>
2022-09-02 07:14:23 -0400

    adding message area

--- Notepad-Angular/src/app/pages/note-details/note-details.component.ts
+++ Notepad-Angular/src/app/pages/note-details/note-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Note } from 'src/shared/note.model';
 
 @Component({
   selector: 'app-note-details',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteDetailsComponent implements OnInit {
 
-  constructor() { }
+  note: Note = new Note;
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
+   
   }
+  
+   clearFields(){
+     this.note.title='';
+     this.note.body='';
+   }
+
+   onSubmit(form: NgForm){
+    console.log(form);
+   }
 
 }
